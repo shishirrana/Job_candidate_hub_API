16f2863e39b2fca8c46964078e40f68c7a91abc0
shishirrana <shishirrana111@gmail.com>
2024-06-19 17:49:04 +0545

    Added migration to successfully deploy the project

---
+++ Sigma.DB/Migrations/20240619115837_init.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sigma.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidates",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CallTimeInterval = table.Column<TimeOnly>(type: "time", nullable: false),
+                    LinkedInURL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GitHubURL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FreeTextComment = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidates", x => x.Email);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidates");
+        }
+    }
+}
